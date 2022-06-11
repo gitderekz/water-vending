f419f1327e917c30ef331075e032ea06bb1cab2b
gitderekz <71731462+gitderekz@users.noreply.github.com>
2022-06-11 16:23:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,301 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="assets/img/favicons/favicon.ico">
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/dashboard/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="dashboard.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+      <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Water vending</a>
+      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+      <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+          <a class="nav-link" href="#">Sign out</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  <span data-feather="home"></span>
+                  Sales <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="clients.php">
+                  <span data-feather="file"></span>
+                  Clients
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="shopping-cart"></span>
+                  Machines
+                </a>
+              </li>
+              <!-- <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="users"></span>
+                  Customers
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="bar-chart-2"></span>
+                  Reports
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="layers"></span>
+                  Integrations
+                </a>
+              </li> -->
+            </ul>
+
+            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+              <span>Saved reports</span>
+              <a class="d-flex align-items-center text-muted" href="#">
+                <span data-feather="plus-circle"></span>
+              </a>
+            </h6>
+            <ul class="nav flex-column mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Current month
+                </a>
+              </li>
+              <!-- <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Last quarter
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Social engagement
+                </a>
+              </li> -->
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  <span data-feather="file-text"></span>
+                  Year-end sale
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+
+
+          <h2>SALES MANAGEMENT</h2>
+
+                              <div class="table-responsive">
+                    <table class="table table-striped table-sm">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Date</th>
+                          <th>Card</th>
+                          <th>Volume</th>
+                          <th>Amount</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                                            <tr>
+                      <td>1</td>
+                      <td>2022-05-12 11:01:54</td>
+                      <td>0007</td>
+                      <td>10</td>
+                      <td>6000</td>
+                      <!-- <td>
+                        <a href="index.php?edit=1" class="btn btn-info">EDIT</a>
+                        <a href="process.php?delete=1" class="btn btn-danger">DELETE</a>
+                      </td> -->
+                    </tr>
+                                      <tr>
+                      <td>2</td>
+                      <td>2022-05-12 11:19:37</td>
+                      <td>7766</td>
+                      <td>10</td>
+                      <td>1000</td>
+                      <!-- <td>
+                        <a href="index.php?edit=2" class="btn btn-info">EDIT</a>
+                        <a href="process.php?delete=2" class="btn btn-danger">DELETE</a>
+                      </td> -->
+                    </tr>
+                                      <tr>
+                      <td>3</td>
+                      <td>2022-05-12 11:19:57</td>
+                      <td>3665</td>
+                      <td>20</td>
+                      <td>500</td>
+                      <!-- <td>
+                        <a href="index.php?edit=3" class="btn btn-info">EDIT</a>
+                        <a href="process.php?delete=3" class="btn btn-danger">DELETE</a>
+                      </td> -->
+                    </tr>
+                                      <tr>
+                      <td>4</td>
+                      <td>2022-05-12 11:20:28</td>
+                      <td>18023</td>
+                      <td>20</td>
+                      <td>500</td>
+                      <!-- <td>
+                        <a href="index.php?edit=4" class="btn btn-info">EDIT</a>
+                        <a href="process.php?delete=4" class="btn btn-danger">DELETE</a>
+                      </td> -->
+                    </tr>
+                                      <tr>
+                      <td>5</td>
+                      <td>2022-05-18 09:49:08</td>
+                      <td>0980</td>
+                      <td>10</td>
+                      <td>8000</td>
+                      <!-- <td>
+                        <a href="index.php?edit=5" class="btn btn-info">EDIT</a>
+                        <a href="process.php?delete=5" class="btn btn-danger">DELETE</a>
+                      </td> -->
+                    </tr>
+                                      <tr>
+                      <td>6</td>
+                      <td>2022-06-05 00:29:12</td>
+                      <td>555</td>
+                      <td>10</td>
+                      <td>500</td>
+                      <!-- <td>
+                        <a href="index.php?edit=6" class="btn btn-info">EDIT</a>
+                        <a href="process.php?delete=6" class="btn btn-danger">DELETE</a>
+                      </td> -->
+                    </tr>
+                                      <tr>
+                      <td>7</td>
+                      <td>2022-06-05 00:32:31</td>
+                      <td>0007</td>
+                      <td>20</td>
+                      <td>1000</td>
+                      <!-- <td>
+                        <a href="index.php?edit=7" class="btn btn-info">EDIT</a>
+                        <a href="process.php?delete=7" class="btn btn-danger">DELETE</a>
+                      </td> -->
+                    </tr>
+                                </tbody>
+            </table>
+          </div>
+
+            <!-- <h1 class="h2">SALES CHART</h1>
+          <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <div class="btn-toolbar mb-2 mb-md-0">
+              *<div class="btn-group mr-2">
+                <button class="btn btn-sm btn-outline-secondary">Share</button>
+                <button class="btn btn-sm btn-outline-secondary">Export</button>
+              </div>
+              <button class="btn btn-sm btn-outline-secondary dropdown-toggle">
+                <span data-feather="calendar"></span>
+                This week
+              *</button>
+            </div>
+          </div> -->
+
+        </main>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+
+    <!-- Graphs -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.7.1/dist/Chart.min.js"></script>
+    <script>
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+          datasets: [{
+            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff'
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          },
+          legend: {
+            display: false,
+          }
+        }
+      });
+    </script>
+
+
+    <div class="row justify-content-center">
+      <form action="process.php" method="POST">
+        <input type="hidden" name="id" value="0">
+        <div class="form-group">
+          <label>Card Number</label>
+          <input type="text" name="card" class="form-control" value=""placeholder="Card">
+        </div>
+        <div class="form-group">
+          <label>Volume</label>
+          <input type="text" name="volume" class="form-control" value="" placeholder="Volume" >
+        </div>
+        <div class="form-group">
+          <label>Amount</label>
+          <input type="text" name="amount" class="form-control" value="" placeholder="Amount">
+        </div>            
+
+        <div class="form-group">
+                      <button type="submit" name="sales" class="btn btn-primary">Save</button>
+                  </div>
+      </form>
+    </div>
+
+
+  </body>
+</html>
